4ba54d12ca9badcc44a2d7835b541c2b33ca742a
prosumer1001 <prosumer1001@gmail.com>
2020-04-30 20:00:53 +0900

    commit updates

--- rankhospital.R
+++ rankhospital.R
@@ -57,119 +57,60 @@ if(T){  ## Set it up the script
 ###                    ###
 ### FUNCTION LOOK DOWN ###
 
-rankhospital <- function(state = as.character(), 
-                         outcome = as.character(), 
-                         num = "best") {
-## Read outcome data
-if(T){ ## Read outcome data and reshape
-                outcome_df <- import("./data/raw/outcome-of-care-measures.csv", 
-                                     colClasses = "character")
-                ## Select and variables 
-                outcome_df <- outcome_df %>%
-                        select("Hospital Name", 
-                               "City", 
-                               "State", 
-                               "Hospital 30-Day Death (Mortality) Rates from Heart Attack",
-                               "Hospital 30-Day Death (Mortality) Rates from Heart Failure",
-                               "Hospital 30-Day Death (Mortality) Rates from Pneumonia")
-                
-                ## Rename variables to be easier to work with
-                names(outcome_df)[4] <- 'heartAttack'
-                names(outcome_df)[5] <- 'heartFailure'
-                names(outcome_df)[6] <- 'pneumonia'
-                names(outcome_df)[1] <- 'Hospital'
-                names(outcome_df)[2] <- 'City'
-                names(outcome_df)[3] <- 'State'
-                
-                ## Make the DF a TIBBLE for better analysis with TIDYVERSE
-                outcome_df <- as_tibble(outcome_df)
-                
-                ## Remove all "Not Available" characters with NAs
-                outcome_df$heartAttack <- na_if(outcome_df$heartAttack, "Not Available")
-                outcome_df$heartFailure <- na_if(outcome_df$heartFailure, "Not Available")
-                outcome_df$pneumonia <- na_if(outcome_df$pneumonia, "Not Available")
-                
-                ## Make each vector with numbers a numeric
-                outcome_df$heartAttack <- as.numeric(outcome_df$heartAttack)
-                outcome_df$heartFailure <- as.numeric(outcome_df$heartFailure)
-                outcome_df$pneumonia <- as.numeric(outcome_df$pneumonia)
-                
-                ## Reshape TIBBLE to be long
-                outcome_df <- outcome_df %>% 
-                        pivot_longer(
-                                cols = heartAttack:pneumonia,
-                                names_to = 'deathCause', 
-                                values_to = 'deathRate'
-                        )
-                
-                ## Rename Factors for ease of reading
-                level_key <- c(heartAttack = "heart attack", 
-                               heartFailure = "heart failure", 
-                               pneumonia = "pneumonia")
-                outcome_df$deathCause <- recode(outcome_df$deathCause, 
-                                                !!!level_key, 
-                                                .default = NA_character_)
-                outcome_df <- as.list(outcome_df)
-                
-                #str(outcome_df)
-                
-        } ## Read outcome data and reshape
-        
-## Check that state and outcome are valid
-        checkState <- if(state %in% outcome_df$State){
-                as_tibble(outcome_df) %>%
+rankhospital <- function(state, outcome, num = "best"){
+## Read outcome data and reshape
+outcome_df <- import("~/Desktop/outcome.RData")
+## Read outcome data and reshape       
+
+checkState <- if(state %in% outcome_df$State){
+        as_tibble(outcome_df) %>%
                 filter(State == state)
-                } else{
-                        stop("invalid state")
-                        }
+} else{
+        stop("invalid state")
+}
+
+checkCause <- if(outcome %in% outcome_df$deathCause){
+        as_tibble(outcome_df) %>%
+                filter(deathCause == outcome) %>%
+                filter(!is.na(deathRate)) %>%
+                filter(State == state) %>%
+                arrange(deathRate) %>%
+                select(Hospital)
+} else{
+        stop("invalid cause")
+}
+
+rankReturn <- if(num == ){
+        outcome_df %>%
+                filter(!is.na(deathRate)) %>%
+                filter(State == state) %>%
+                filter(deathCause == outcome) %>%
+                arrange(deathRate, Hospital) %>%
+                slice(num)
+} else if(num == as.character("best")){
+                outcome_df %>%
+                filter(!is.na(deathRate)) %>%
+                filter(State == state) %>%
+                filter(deathCause == outcome) %>%
+                arrange(deathRate) %>%
+                select(Hospital) %>%
+                head(1)
+} else if(num == as.character("worst")){
+        as_tibble(outcome_df) %>%
+                filter(!is.na(deathRate)) %>%
+                filter(State == state) %>%
+                filter(deathCause == outcome) %>%
+                arrange(deathRate) %>%
+                select(Hospital) %>%
+                tail(1)
+} else{
+        stop("ifelse does not work")
+}
 
 
-                best <- if(outcome %in% outcome_df$deathCause){
-                        as_tibble(outcome_df) %>%
-                                filter(deathCause == outcome) %>%
-                                filter(!is.na(deathRate)) %>%
-                                filter(State == state) %>%
-                                arrange(deathRate) %>%
-                                select(Hospital)
-                } else{
-                        stop("invalid cause")
-                }
-                
-                worst <- if(outcome %in% outcome_df$deathCause){
-                        as_tibble(outcome_df) %>%
-                                filter(deathCause == outcome) %>%
-                                filter(!is.na(deathRate)) %>%
-                                filter(State == state) %>%
-                                arrange(desc(deathRate)) %>%
-                                select(Hospital)
-                } else{
-                        stop("invalid cause")
-                }
-                
-                ranked <- if(outcome %in% outcome_df$deathCause){
-                        as_tibble(outcome_df) %>%
-                                filter(deathCause == outcome) %>%
-                                filter(!is.na(deathRate)) %>%
-                                filter(State == state) %>%
-                                arrange(deathRate) %>%
-                                slice(num) %>%
-                                select(Hospital)
-                } else{
-                        stop("invalid cause")
-                }
+print(rankReturn)
         
-        ## Return Hospital name in that state with the given rank
-                if(num == "best"){
-                        print(head(best, 1))
-                } else if(num == "worst"){
-                        print(head(worst, 1))
-                } else{
-                        print(ranked)
-                }
-
-        
-}   ## rankhospital() function end.
-
+} ## Function END
 
 ### FUNCTION LOOK ^UP^ ###
 ###                    ###
@@ -209,7 +150,7 @@ rowSums(is.na(outcome))
 
 ## Keep unique functions defined in this script
 rm(list=setdiff(ls(), c("clear", "is.sorted", "packages", "helpCon")))
-
+rm(list = ls())
 ## Clear the Console
 clear()
 
