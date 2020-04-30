429c4f1819b6ae777970fca49cdd793b3dd206e3
prosumer1001 <prosumer1001@gmail.com>
2020-04-30 20:38:44 +0900

    final working function for rankhospital.R

--- rankhospital.R
+++ rankhospital.R
@@ -51,8 +51,6 @@ if(T){  ## Set it up the script
 } ## Set it up the script
 
 
-
-
 ### FUNCTION LOOK DOWN ###
 ###                    ###
 ### FUNCTION LOOK DOWN ###
@@ -60,6 +58,7 @@ if(T){  ## Set it up the script
 rankhospital <- function(state, outcome, num = "best"){
 ## Read outcome data and reshape
 outcome_df <- import("~/Desktop/outcome.RData")
+## str(outcome_df)
 ## Read outcome data and reshape       
 
 checkState <- if(state %in% outcome_df$State){
@@ -74,42 +73,27 @@ checkCause <- if(outcome %in% outcome_df$deathCause){
                 filter(deathCause == outcome) %>%
                 filter(!is.na(deathRate)) %>%
                 filter(State == state) %>%
-                arrange(deathRate) %>%
+                arrange(deathRate, State, Hospital) %>%
                 select(Hospital)
 } else{
         stop("invalid cause")
 }
 
-rankReturn <- if(num == ){
-        outcome_df %>%
-                filter(!is.na(deathRate)) %>%
-                filter(State == state) %>%
-                filter(deathCause == outcome) %>%
-                arrange(deathRate, Hospital) %>%
-                slice(num)
-} else if(num == as.character("best")){
-                outcome_df %>%
-                filter(!is.na(deathRate)) %>%
-                filter(State == state) %>%
-                filter(deathCause == outcome) %>%
-                arrange(deathRate) %>%
-                select(Hospital) %>%
-                head(1)
-} else if(num == as.character("worst")){
-        as_tibble(outcome_df) %>%
-                filter(!is.na(deathRate)) %>%
-                filter(State == state) %>%
-                filter(deathCause == outcome) %>%
-                arrange(deathRate) %>%
-                select(Hospital) %>%
-                tail(1)
-} else{
-        stop("ifelse does not work")
-}
+rankHospital <- outcome_df %>%
+        filter(!is.na(deathRate)) %>%
+        filter(State == state) %>%
+        filter(deathCause == outcome) %>%
+        arrange(deathRate, State, deathCause, Hospital) %>%
+        select(Hospital)
 
+if(num == "best"){
+        head(rankHospital, 1)
+} else if(num == "worst"){
+        tail(rankHospital, 1)
+} else if(num >= 0){
+        slice(rankHospital, num)
+}      
 
-print(rankReturn)
-        
 } ## Function END
 
 ### FUNCTION LOOK ^UP^ ###
@@ -119,14 +103,9 @@ print(rankReturn)
 
 ##### WHITE BOARD #####
 
-rankhospital("MD", "heart attack", "worst")
+rankhospital("TX", "heart failure", 5)
 
 ##### END WHITE BOARD #####
-
-
-
-
-
 
 
 ##### SOME GOOD DESCRIPTIVE STATS CODE #####
@@ -138,9 +117,6 @@ sapply(outcome, function(outcome) sum(is.na(outcome)))
 
 ## If you need NA count Row wise
 rowSums(is.na(outcome))
-
-## WITH TIDYVERSE
-
 
 ##### END GOOD DS CODE #####
 
